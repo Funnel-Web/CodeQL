bda9a7f3a13eb694716ed79fb9dc1b7604927552
James Scott <jim_a_scott@hotmail.com>
2023-10-11 16:25:46 +0100

    feat: trying to get a faile

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { failMe } from './lib/codeLib'
+import { failMe } from './lib/codeLib';
+import { failMe as faileMe2 } from './lib/codeLib2';
 function codeTest() {
     console.log('This is a bit of a test and a change here');
     console.log(' The password is "f4k3"');
-    failMe('Oh Come on')
+    failMe('Oh Come on');
+    faileMe2('and again code repetition here');
 }
 
 codeTest();
