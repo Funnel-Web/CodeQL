c291d81a1f3a37ef7476a635ad0241163929cc4f
James Scott <jim_a_scott@hotmail.com>
2023-10-11 16:24:35 +0100

    feat: add in more code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { failMe } from './lib/codeLib'
 function codeTest() {
     console.log('This is a bit of a test and a change here');
-
     console.log(' The password is "f4k3"');
+    failMe('Oh Come on')
 }
 
 codeTest();
