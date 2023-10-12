01cbb659cb77aca7d3a9d8c860ab5840c0d53266
James Scott <jim_a_scott@hotmail.com>
2023-10-12 09:48:30 +0100

    feat: update the code (#1)
    * feat: update the code
    
    * feat: add a password?
    
    * feat: add in more code
    
    * feat: trying to get a faile
    
    * feat: dirty code
    
    * feat: come on breakgit add -A!

---
+++ src/lib/codeLib.ts
@@ -0,0 +1,5 @@
+export function failMe(input: string): void {
+    //Trying to get something to fail here. 
+
+    console.log(`faileme: ${input}`);
+}
---
+++ src/lib/codeLib2.ts
@@ -0,0 +1,12 @@
+export function failMe(input: string): void {
+    //Trying to get something to fail here. 
+
+    let something = 'hello'
+
+    const loc = null;
+    if (dist < 10)
+        loc = "here";
+    else
+        loc = "there";
+    console.log(`faileme: ${input} and ${loc} and ${something}`);
+}
