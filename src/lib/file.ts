228abf060d3f60890570004c17d12c38d2a19bb0
James Scott <jim_a_scott@hotmail.com>
2023-10-12 17:18:14 +0100

    feat: adding a pssword in?

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -2,5 +2,13 @@ import fs from 'fs-extra';
 
 export async function writeFile(contents: string) {
     // TODO: do something better here
-    await fs.writeFile('a-file.txt', contents);
+    const username = 'user@name.com';
+    const password = 'abs&1234';
+    const body = `
+    UN: ${username}
+    PW: ${password}
+    COMTETS:
+    ${contents}
+    `
+    await fs.writeFile('a-file.txt', body);
 }
