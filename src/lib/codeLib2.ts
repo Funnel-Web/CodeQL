edea74501ef22d0a69195d9aafd3c484ae4697e6
James Scott <jim_a_scott@hotmail.com>
2023-10-12 10:29:14 +0100

    feat: adding in duplication

--- src/lib/codeLib2.ts
+++ src/lib/codeLib2.ts
@@ -1,12 +1,5 @@
 export function failMe(input: string): void {
     //Trying to get something to fail here. 
 
-    let something = 'hello'
-
-    let loc = null;
-    if (input === '10')
-        loc = "here";
-    else
-        loc = "there";
-    console.log(`faileme: ${input} and ${loc} and ${something}`);
+    console.log(`faileme: ${input}`);
 }
