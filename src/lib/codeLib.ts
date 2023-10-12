85e37a23a2533a5662f6386c3cbe326715059486
James Scott <jim_a_scott@hotmail.com>
2023-10-12 15:14:46 +0100

    test: add some testing in

--- src/lib/codeLib.ts
+++ src/lib/codeLib.ts
@@ -15,3 +15,15 @@ export function failMe(input: string): void {
         console.log(`faileme: ${input} with the ${addition}`);
     }
 }
+
+export function convertBytes(input: number): string {
+    const units = ['byte', 'bytes', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+    let l = 0, n = input;
+    if (input != 1) {
+        ++l;
+        while (n >= 1024 && ++l) {
+            n = n / 1024;
+        }
+    }
+    return `${n.toFixed(1).replace('.0', '')} ${units[l]}`;
+}
