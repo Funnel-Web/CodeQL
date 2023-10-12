95f3c7e80f7bd212f3f6bf502f7185471b8c77e6
James Scott <jim_a_scott@hotmail.com>
2023-10-12 10:33:22 +0100

    feat: addition some duplication

--- src/lib/codeLib2.ts
+++ src/lib/codeLib2.ts
@@ -1,5 +1,17 @@
 export function failMe(input: string): void {
     //Trying to get something to fail here. 
-
-    console.log(`faileme: ${input}`);
+    let addition;
+    switch (input) {
+        case 'input1':
+            addition = 'another thing';
+        case 'input2':
+            addition = 'something else';
+        case 'input3':
+            addition = 'one more thing';
+    }
+    if (addition) {
+        console.log(`faileme: ${input} with the ${addition}`);
+    } else {
+        console.log(`faileme: ${input} with the ${addition}`);
+    }
 }
